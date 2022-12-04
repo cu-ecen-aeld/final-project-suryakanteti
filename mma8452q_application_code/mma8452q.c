fc87f56723f936579ae1c81da3a2fb77a049c50d
Peter Braganza <peterbraganza52@gmail.com>
2022-12-03 19:15:55 -0700

    accl x,y,z added

--- mma8452q_application_code/mma8452q.c
+++ mma8452q_application_code/mma8452q.c
@@ -1,6 +1,3 @@
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <linux/i2c-dev.h>
@@ -15,14 +12,22 @@
 #define MMA8452Q_FILE "/dev/i2c-2"
 #define MMA8452Q_ADDR 0x1D
 #define WHO_AM_I 0x0D
+#define CTRL_REG 0x2A
+
+uint8_t ctrl_reg[2] = {CTRL_REG, 0x01};
+uint8_t out_x_msb = 0x01;
+
 
 int main()
 {
 
     int ret_val = 0;
-    uint16_t who_am_i_reg = WHO_AM_I;
-    char buf[2];
-    uint16_t reg_val;
+    uint8_t who_am_i_reg = WHO_AM_I;
+    uint8_t buf[2];
+    uint8_t reg_val;
+    uint8_t accl[6];
+
+    int16_t acc_x, acc_y, acc_z;     //+/-2g
 
 
     int i2c_fd = open(MMA8452Q_FILE, O_RDWR);
@@ -41,7 +46,31 @@ int main()
     if(ret_val != sizeof(who_am_i_reg))
         perror("read()");
     
-    reg_val = buf[1] | (buf[0] << 8);
-    printf("Who am I reg: 0x%02x", reg_val);
+    //reg_val = buf[1] | (buf[0] << 8);
+    //reg_val = buf[0];
+    
+    printf("Who am I reg: 0x%02x", buf[0]);
+
+    ret_val = write(i2c_fd, &ctrl_reg, sizeof(ctrl_reg));
+    if(ret_val != sizeof(ctrl_reg))
+        perror("write()");
+
+
+    ret_val = write(i2c_fd, &out_x_msb, sizeof(uint8_t));
+    if(ret_val != sizeof(out_x_msb))
+        perror("write()");
+
+    ret_val = read(i2c_fd, accl, sizeof(accl));
+    if(ret_val != sizeof(accl))
+        perror("read()");
+
+    acc_x = (accl[0] << 8) | accl[1];
+    acc_y = (accl[2] << 8) | accl[3];
+    acc_z = (accl[4] << 8) | accl[5];
+
+    printf("X: %d, Y: %d, Z: %d\r\n", acc_x/1024, acc_y/1024, acc_y/1024);
+
+
+    return 0;
 
 }

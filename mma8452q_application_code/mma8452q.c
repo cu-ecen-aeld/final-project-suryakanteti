24c9c1e42f445b6bfa0742e12005f59f33840528
Peter Braganza <peterbraganza52@gmail.com>
2022-12-03 20:10:33 -0700

    mininal test code

--- mma8452q_application_code/mma8452q.c
+++ mma8452q_application_code/mma8452q.c
@@ -22,7 +22,7 @@ int main()
 {
 
     int ret_val = 0;
-    uint8_t who_am_i_reg = WHO_AM_I;
+    uint8_t who_am_i_reg = 0x0D;
     uint8_t buf[2];
     uint8_t reg_val;
     uint8_t accl[6];
@@ -38,12 +38,12 @@ int main()
     if(ret_val < 0)
     perror("ioctl()");
 
-    ret_val = write(i2c_fd, &who_am_i_reg, sizeof(who_am_i_reg));
-    if(ret_val != sizeof(who_am_i_reg))
+    ret_val = write(i2c_fd, &who_am_i_reg, 1);
+    if(ret_val != 1)
         perror("write()");
 
-    ret_val = read(i2c_fd, buf, sizeof(who_am_i_reg));
-    if(ret_val != sizeof(who_am_i_reg))
+    ret_val = read(i2c_fd, buf, 1);
+    if(ret_val != 1)
         perror("read()");
     
     //reg_val = buf[1] | (buf[0] << 8);
@@ -51,33 +51,33 @@ int main()
     
     printf("Who am I reg: 0x%02x\n", buf[0]);
 
-    ret_val = write(i2c_fd, &ctrl_reg, sizeof(ctrl_reg));
-    if(ret_val != sizeof(ctrl_reg))
-        perror("write()");
+    // ret_val = write(i2c_fd, &ctrl_reg, 1);
+    // if(ret_val != sizeof(ctrl_reg))
+    //     perror("write()");
 
-    while(1){
+    // while(1){
 
-        ret_val = read(i2c_fd, buf, sizeof(who_am_i_reg));
-        if(ret_val != sizeof(who_am_i_reg))
-            perror("read()");
+    //     ret_val = read(i2c_fd, buf, sizeof(who_am_i_reg));
+    //     if(ret_val != sizeof(who_am_i_reg))
+    //         perror("read()");
     
-        printf("Who am I reg: 0x%02x\n", buf[0]);
-        ret_val = write(i2c_fd, &out_x_msb, sizeof(uint8_t));
-        if(ret_val != sizeof(out_x_msb))
-            perror("write()");
-            
+    //     printf("Who am I reg: 0x%02x\n", buf[0]);
+    //     ret_val = write(i2c_fd, &out_x_msb, sizeof(uint8_t));
+    //     if(ret_val != sizeof(out_x_msb))
+    //         perror("write()");
 
-        ret_val = read(i2c_fd, accl, sizeof(accl));
-        if(ret_val != sizeof(accl))
-            perror("read()");
 
-        acc_x = (accl[0] << 8) | accl[1];
-        acc_y = (accl[2] << 8) | accl[3];
-        acc_z = (accl[4] << 8) | accl[5];
+    //     ret_val = read(i2c_fd, accl, sizeof(accl));
+    //     if(ret_val != sizeof(accl))
+    //         perror("read()");
 
-        printf("X: %d, Y: %d, Z: %d\r\n", acc_x/1024, acc_y/1024, acc_y/1024);
+    //     acc_x = (accl[0] << 8) | accl[1];
+    //     acc_y = (accl[2] << 8) | accl[3];
+    //     acc_z = (accl[4] << 8) | accl[5];
 
-    }
+    //     printf("X: %d, Y: %d, Z: %d\r\n", acc_x/1024, acc_y/1024, acc_y/1024);
+
+    // }
 
     return 0;
 

db81da454aa6e59a1cecb3afd7f4bb7db764fca9
Peter Braganza <peterbraganza52@gmail.com>
2022-12-04 19:35:05 -0700

    Initial commit for sensor integration

--- server/server.c
+++ server/server.c
@@ -15,11 +15,20 @@
 #include<netinet/in.h>
 #include <string.h>
 
+// Sensor headers
+#include "../gps_application_code/gps.h"
+#include "../mma8452q_application_code/mma8452.h"
+
 #define PORT "9000"
 #define BACKLOG (5)
 #define BUFFER_SIZE (1024)
 struct addrinfo *server_info=NULL;
 bool interrupted=false;
+
+// Sensor params
+#define SENSOR_DATA_LENGTH 512
+int gpsFd = -1;
+int acclFd = -1;
 
 static void signal_handler (int signo)
 {
@@ -45,7 +54,12 @@ struct addrinfo hints;
 socklen_t address_len=sizeof(struct sockaddr);
 socklen_t addr_size=sizeof(client_addr);
 int sockfd,new_sockfd,s_send;
-message="4000.81269 N 0515.92189 W 234942.00";
+//message="4000.81269 N 0515.92189 W 234942.00";
+
+// Open sensor ports
+gpsFd = OpenPort();
+acclFd = open_i2c_port();
+
 if(((sockfd=socket(PF_INET,SOCK_STREAM,0)))==-1){
     syslog(LOG_USER, "Not able to create socket");
     perror("socket");
@@ -76,21 +90,39 @@ freeaddrinfo(server_info);
 syslog(LOG_USER, "Listening");
 listen(sockfd,BACKLOG);
 
-while(!interrupted){ 
-new_sockfd=accept(sockfd,&client_addr,&addr_size);
+char sensorData[SENSOR_DATA_LENGTH];
 
-if(new_sockfd==-1){
-    syslog(LOG_USER, "Error while accepting");
-    perror("accept");
-    close(sockfd);
-    return(-1);
-}
-syslog(LOG_USER, "Accepted Connection");
-inet_ntop(AF_INET,&client_addr,clientIP,sizeof(clientIP));
+while(!interrupted)
+{ 
+    new_sockfd=accept(sockfd,&client_addr,&addr_size);
+
+    if(new_sockfd==-1)
+    {
+        syslog(LOG_USER, "Error while accepting");
+        perror("accept");
+        close(sockfd);
+        return(-1);
+    }
+
+    syslog(LOG_USER, "Accepted Connection");
+    inet_ntop(AF_INET,&client_addr,clientIP,sizeof(clientIP));
     syslog(LOG_USER, "Sending message:%s",message);
-    if((s_send=send(new_sockfd,message,(strlen(message)/sizeof(char)),0))<0){
+
+    // Populate the sensor data
+    int gpsBytes = PopulateGpsData(gpsFd, sensorData, SENSOR_DATA_LENGTH);
+    int acclBytes = populate_accl_data(sensorData + gpsBytes, SENSOR_DATA_LENGTH - gpsBytes, acclFd);
+    sensorData[gpsBytes + acclBytes] = '\0';
+    sensorData[gpsBytes + acclBytes + 1] = '\n';
+
+    if((s_send=send(new_sockfd,sensorData,(strlen(sensorData)/sizeof(char)),0))<0)
+    {
         syslog(LOG_USER, "Sending failed"); 
-    } 
+    }
+    
+    /*if((s_send=send(new_sockfd,message,(strlen(message)/sizeof(char)),0))<0)
+    {
+        syslog(LOG_USER, "Sending failed"); 
+    }*/
 } 
 close(sockfd);          
 }
